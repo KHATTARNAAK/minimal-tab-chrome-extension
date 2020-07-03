476bd42b453c6be209b7ae37156465ceb6cb2b93
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-07-03 23:33:02 +0530

    Added dark-light mode switcher

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ function showTime() {
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
+
+  if (hours > 12) {
+    hours -= 12;
+  }
+
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
@@ -12,3 +17,19 @@ function showTime() {
 }
 
 setInterval(showTime, 1000);
+
+const colorButton = document.querySelector(".color-button");
+const colorDiv = colorButton.parentElement;
+colorButton.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  if (colorButton.innerHTML === '<i class="fas fa-bolt"></i>') {
+    // removing dark mode
+    document.body.classList.replace("dark-mode", "light-mode");
+    colorButton.innerHTML = '<i class="fas fa-moon"></i>';
+  } else {
+    // removing light mode
+    document.body.classList.replace("light-mode", "dark-mode");
+    colorButton.innerHTML = '<i class="fas fa-bolt"></i>';
+  }
+}
